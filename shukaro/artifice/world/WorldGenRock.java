1d424c184707623d08b87611e254c09a3abfdac6
Shukaro <shukaro@gmail.com>
2013-05-25 11:18:35 -0400

    Revamp world generation

--- shukaro/artifice/world/WorldGenRock.java
+++ shukaro/artifice/world/WorldGenRock.java
@@ -12,50 +12,81 @@ import shukaro.artifice.util.BlockCoord;
 
 public class WorldGenRock
 {
-    private static int x1;
-    private static int y1;
-    private static int z1;
-    private static BlockCoord coord = new BlockCoord();
-    private static List<BlockCoord> primary = new ArrayList<BlockCoord>();
-    private static List<BlockCoord> secondary = new ArrayList<BlockCoord>();
+    private static World world;
+    private static Random rand;
+    private static int startX;
+    private static int startY;
+    private static int startZ;
+    private static int id;
+    private static int meta;
+    private static int size;
+    private static List<Integer> replaced;
+    private static int maxHeight;
     
-    public static boolean generate(World world, Random rand, int x, int y, int z, int id, int meta, int size, List<Integer> replaced, int maxHeight)
+    private static int maxX;
+    private static int minX;
+    private static int maxZ;
+    private static int minZ;
+    
+    private static int maxY;
+    private static int minY;
+    
+    private static BlockCoord coord = new BlockCoord();
+    private static List<BlockCoord> list = new ArrayList<BlockCoord>();
+    
+    public WorldGenRock(World world, Random rand, int x, int y, int z, int id, int size, int maxHeight)
     {
-        primary.clear();
-        secondary.clear();
+        this(world, rand, x, y, z, id, 0, size, ArtificeRegistry.getStoneTypes(), maxHeight);
+    }
+    
+    public WorldGenRock(World world, Random rand, int x, int y, int z, int id, int meta, int size, List<Integer> replaced, int maxHeight)
+    {
+        this.world = world;
+        this.rand = rand;
+        this.startX = x;
+        this.startY = y;
+        this.startZ = z;
+        this.id = id;
+        this.meta = meta;
+        this.size = size;
+        this.replaced = replaced;
+        this.maxHeight = maxHeight;
+    }
+    
+    public boolean generate()
+    {
+        list.clear();
         
         for (int i = 0; i < rand.nextInt(6) + 4; i++)
         {
-            x1 = x + rand.nextInt(8) - rand.nextInt(8);
-            y1 = y + rand.nextInt(4) - rand.nextInt(4);
-            z1 = z + rand.nextInt(8) - rand.nextInt(8);
-            coord.set(x1, y1, z1);
+            coord.x = startX + rand.nextInt(8) - rand.nextInt(8);
+            coord.y = startY + rand.nextInt(4) - rand.nextInt(4);
+            coord.z = startZ + rand.nextInt(8) - rand.nextInt(8);
             
-            if (canGenHere(world, coord, false, replaced, id) && coord.y < maxHeight)
+            this.maxX = coord.x + 16;
+            this.minX = coord.x - 16;
+            this.maxZ = coord.z + 16;
+            this.minZ = coord.z - 16;
+            
+            this.maxY = coord.y + rand.nextInt(32);
+            this.minY = coord.y - rand.nextInt(32);
+            
+            if (canGenHere(coord) && coord.y < maxHeight)
             {
                 world.setBlock(coord.x, coord.y, coord.z, id, meta, 2);
                 for (int j = 0; j < size; j++)
                 {
                     for (BlockCoord c : coord.getAdjacent())
                     {
-                        if (canGenHere(world, c, false, replaced, id))
-                            primary.add(c);
-                        if (canGenHere(world, c, true, replaced, id))
-                            secondary.add(c);
+                        if (canGenHere(c))
+                            list.add(c);
                     }
-                    if (primary.isEmpty() && secondary.isEmpty())
+                    
+                    if (list.size() == 0)
                         return true;
-                    else if (primary.isEmpty())
-                        coord.set(secondary.get(rand.nextInt(secondary.size())));
-                    else if (secondary.isEmpty())
-                        coord.set(primary.get(rand.nextInt(primary.size())));
-                    else
-                    {
-                        if (rand.nextInt(100) < 60)
-                            coord.set(primary.get(rand.nextInt(primary.size())));
-                        else
-                            coord.set(secondary.get(rand.nextInt(secondary.size())));
-                    }
+                    
+                    coord.set(list.get(rand.nextInt(list.size())));
+                    list.remove(coord);
                     world.setBlock(coord.x, coord.y, coord.z, id, meta, 2);
                 }
                 return true;
@@ -65,21 +96,26 @@ public class WorldGenRock
         return true;
     }
     
-    private static boolean canGenHere(World world, BlockCoord c, boolean includeSelf, List<Integer> replaced, int id)
+    private boolean canGenHere(BlockCoord c)
     {
+        if (c.x > this.maxX || c.x < this.minX)
+            return false;
+        
+        if (c.y > this.maxY || c.y < this.minY)
+            return false;
+        
+        if (c.z > this.maxZ || c.z < this.minZ)
+            return false;
+        
+        if (Block.blocksList[world.getBlockId(c.x, c.y, c.z)] == null)
+            return false;
+        
         for (int i : replaced)
         {
             if (i == world.getBlockId(c.x, c.y, c.z))
-            {
-                for (BlockCoord near : c.getNearby())
-                {
-                    if (Block.blocksList[world.getBlockId(near.x, near.y, near.z)] == null || !Block.blocksList[world.getBlockId(near.x, near.y, near.z)].isOpaqueCube())
-                        return true;
-                    if (includeSelf && world.getBlockId(near.x, near.y, near.z) == id)
-                        return true;
-                }
-            }
+                return true;
         }
+        
         return false;
     }
 }

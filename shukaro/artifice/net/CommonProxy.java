e261cd5fc95d5247da983782ea9ce1c998e20e85
Ross Swartz <ross.swartz@gmail.com>
2013-09-09 19:39:05 -0400

    Update gitignore, remove unused multiblock code for now

--- shukaro/artifice/net/CommonProxy.java
+++ shukaro/artifice/net/CommonProxy.java
@@ -1,7 +1,6 @@
 package shukaro.artifice.net;
 
 import shukaro.artifice.event.WorldTicker;
-import shukaro.artifice.multiblock.erogenousbeef.MultiblockServerTickHandler;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 
@@ -10,6 +9,5 @@ public class CommonProxy
     public static void init()
     {
         TickRegistry.registerTickHandler(new WorldTicker(), Side.SERVER);
-        TickRegistry.registerTickHandler(new MultiblockServerTickHandler(), Side.SERVER);
     }
 }

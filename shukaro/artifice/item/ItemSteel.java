556d7997767d238ac9c94162870ef6bdb1f420b8
Shukaro <shukaro@gmail.com>
2013-09-11 23:10:13 -0400

    Add alternate steel recipe

--- shukaro/artifice/item/ItemSteel.java
+++ shukaro/artifice/item/ItemSteel.java
@@ -10,6 +10,7 @@ import shukaro.artifice.compat.ArtificeRegistry;
 import shukaro.artifice.gui.ArtificeCreativeTab;
 import shukaro.artifice.util.IdMetaPair;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -18,11 +19,13 @@ import net.minecraft.util.Icon;
 public class ItemSteel extends Item
 {
     public Icon icon;
+    public Icon dustIcon;
+    private String[] names = { "ingot", "dust" };
     
     public ItemSteel(int id)
     {
         super(id);
-        this.setUnlocalizedName("artifice.ingot.steel");
+        this.setUnlocalizedName("artifice.steel");
         this.setCreativeTab(ArtificeCreativeTab.main);
     }
     
@@ -30,14 +33,34 @@ public class ItemSteel extends Item
     @SideOnly(Side.CLIENT)
     public Icon getIconFromDamage(int meta)
     {
-        return icon;
-    }
+    	if (meta == 0)
+    		return icon;
+    	else
+    		return dustIcon;
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack stack)
+	{
+		if (stack.getItemDamage() > 1)
+			return "item.artifice.steel." + names[0].toLowerCase();
+		return "item.artifice.steel." + names[stack.getItemDamage()].toLowerCase();
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(int id, CreativeTabs tab, List list)
+	{
+		list.add(new ItemStack(id, 1, 0));
+		list.add(new ItemStack(id, 1, 1));
+ 	}
     
     @Override
     @SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister reg)
     {
         this.icon = reg.registerIcon("artifice:ingot_steel");
+        this.dustIcon = reg.registerIcon("artifice:dust_steel");
     }
     
     @Override
@@ -46,7 +69,7 @@ public class ItemSteel extends Item
     {
         if (!ArtificeConfig.tooltips.getBoolean(true))
             return;
-        IdMetaPair pair = new IdMetaPair(stack.itemID, 0);
+        IdMetaPair pair = new IdMetaPair(stack.itemID, stack.getItemDamage());
         if (ArtificeRegistry.getTooltipMap().get(pair) != null)
         {
             for (String s : ArtificeRegistry.getTooltipMap().get(pair))

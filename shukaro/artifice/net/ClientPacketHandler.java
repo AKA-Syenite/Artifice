d98b5b41fab5a237560e896a2254e763fd2907cf
Shukaro <shukaro@gmail.com>
2013-10-14 19:03:18 -0400

    Fix gitignore

---
+++ shukaro/artifice/net/ClientPacketHandler.java
@@ -0,0 +1,20 @@
+package shukaro.artifice.net;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+
+import shukaro.artifice.util.PacketWrapper;
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet250CustomPayload;
+import cpw.mods.fml.common.network.IPacketHandler;
+import cpw.mods.fml.common.network.Player;
+
+public class ClientPacketHandler implements IPacketHandler
+{
+	@Override
+	public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player)
+	{
+		DataInputStream data = new DataInputStream(new ByteArrayInputStream(packet.data));
+		int packetType = PacketWrapper.readPacketID(data);
+	}
+}
---
+++ shukaro/artifice/net/PlayerTracking.java
@@ -0,0 +1,9 @@
+package shukaro.artifice.net;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerTracking
+{
+	public static List<Integer> sneaks = new ArrayList<Integer>();
+}

dc0b284db654af5a2c695b02ee33f86b7b07b599
Ross Swartz <ross.swartz@gmail.com>
2013-10-15 00:51:08 -0400

    Fix key handling

--- shukaro/artifice/net/ClientProxy.java
+++ shukaro/artifice/net/ClientProxy.java
@@ -1,14 +1,11 @@
 package shukaro.artifice.net;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
 import shukaro.artifice.ArtificeConfig;
 import shukaro.artifice.ArtificeCore;
-import shukaro.artifice.event.ArtificeKeyHandler;
+import shukaro.artifice.event.KeyTicker;
 import shukaro.artifice.event.ScaffoldTicker;
 import shukaro.artifice.render.FrameRenderer;
 import shukaro.artifice.render.LotusRenderer;
-import cpw.mods.fml.client.registry.KeyBindingRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -17,17 +14,13 @@ public class ClientProxy extends CommonProxy
 {
     public static void init()
     {
-        ArtificeCore.frameRenderID = RenderingRegistry.getNextAvailableRenderId();
         ArtificeCore.lotusRenderID = RenderingRegistry.getNextAvailableRenderId();
-        RenderingRegistry.registerBlockHandler(ArtificeCore.frameRenderID, new FrameRenderer());
         RenderingRegistry.registerBlockHandler(ArtificeCore.lotusRenderID, new LotusRenderer());
+        ArtificeCore.frameRenderID = RenderingRegistry.getNextAvailableRenderId();
+        RenderingRegistry.registerBlockHandler(ArtificeCore.frameRenderID, new FrameRenderer());
         
         if (ArtificeConfig.enableBoxes.getBoolean(true))
-        {
-	        KeyBinding[] sneak = {new KeyBinding("Sneak", Minecraft.getMinecraft().gameSettings.keyBindSneak.keyCode)};
-	        boolean[] repeats = {false};
-	        KeyBindingRegistry.registerKeyBinding(new ArtificeKeyHandler(sneak, repeats));
-        }
+	        TickRegistry.registerTickHandler(new KeyTicker(), Side.CLIENT);
         
         if (ArtificeConfig.enableFrames.getBoolean(true))
             TickRegistry.registerTickHandler(new ScaffoldTicker(), Side.CLIENT);

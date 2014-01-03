2c7617331bc2828c3765b6e99255c584bd8d128f
Shukaro <shukaro@gmail.com>
2014-01-02 21:15:31 -0500

    Fix scaffold climbing for the most part

--- shukaro/artifice/net/ClientProxy.java
+++ shukaro/artifice/net/ClientProxy.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import shukaro.artifice.ArtificeConfig;
 import shukaro.artifice.ArtificeCore;
 import shukaro.artifice.event.KeyTicker;
-import shukaro.artifice.event.ScaffoldTicker;
 import shukaro.artifice.render.FrameRenderer;
 import shukaro.artifice.render.LotusRenderer;
 import shukaro.artifice.util.BlockCoord;
@@ -24,9 +23,6 @@ public class ClientProxy extends CommonProxy
         
         if (ArtificeConfig.enableBoxes.getBoolean(true))
 	        TickRegistry.registerTickHandler(new KeyTicker(), Side.CLIENT);
-        
-        if (ArtificeConfig.enableFrames.getBoolean(true))
-            TickRegistry.registerTickHandler(new ScaffoldTicker(), Side.CLIENT);
         
         ArtificeCore.textureCache = new ConcurrentHashMap<BlockCoord, int[]>();
     }

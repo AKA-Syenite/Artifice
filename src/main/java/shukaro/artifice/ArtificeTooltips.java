1c18ec8c36c77bee3bf34ba034b177339d008de7
Shukaro <shukaro@gmail.com>
2014-01-23 16:48:12 -0500

    Modify some more tooltip formatting

--- src/main/java/shukaro/artifice/ArtificeTooltips.java
+++ src/main/java/shukaro/artifice/ArtificeTooltips.java
@@ -248,51 +248,51 @@ public class ArtificeTooltips
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockFrame.blockID, 3, commentCode + "...anything is possible");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 0, "Reinforced Glass");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 0, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(0));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 0, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(0));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 0, commentCode + "Strengthening glass is tricky");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 1, "Ceramic Glass");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 1, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(1));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 1, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(1));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 1, commentCode + "Ceramics make the world go round");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 2, "Rock Glass");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 2, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(2));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 2, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(2));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 2, commentCode + "Volcanic glass is naturally strong");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 3, "Laminated Glass");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 3, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(3));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 3, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockGlassWall.getResistance(3));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockGlassWall.blockID, 3, commentCode + "Filled with thin steel threads");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 0, "Reinforced Stone Bricks");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 0, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(0));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 0, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(0));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 0, commentCode + "A good first attempt");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 1, "Reinforced Bricks");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 1, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(1));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 1, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(1));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 1, commentCode + "Hardened bricks, eh?");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 2, "Obsidian Bricks");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 2, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(2));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 2, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(2));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 2, commentCode + "There is always progress to be made...");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 3, "Steel Plating");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 3, "Resistance: " + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(3));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 3, FormatCodes.Italic.code + "Resistance: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + (int) ArtificeBlocks.blockReinforced.getResistance(3));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockReinforced.blockID, 3, commentCode + "...and progress always provides");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 0, "Simple scaffold");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 0, "Max Overhang: " + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(0));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 0, FormatCodes.Italic.code + "Max Overhang: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(0));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 0, commentCode + "Handy to have");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 1, "Composite scaffold");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 1, "Max Overhang: " + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(1));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 1, FormatCodes.Italic.code + "Max Overhang: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(1));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 1, commentCode + "A bit less rickety");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 2, "Construction scaffold");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 2, "Max Overhang: " + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(2));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 2, FormatCodes.Italic.code + "Max Overhang: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(2));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 2, commentCode + "100% safe");
             
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 3, "Rugged scaffold");
-            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 3, "Max Overhang: " + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(3));
+            ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 3, FormatCodes.Italic.code + "Max Overhang: " + FormatCodes.Reset.code + FormatCodes.Aqua.code + ArtificeBlocks.blockScaffold.getOverhang(3));
             ArtificeRegistry.registerTooltip(ArtificeBlocks.blockScaffold.blockID, 3, commentCode + "Precisely machined");
         }
 

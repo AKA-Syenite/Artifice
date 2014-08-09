a544746e2a9681b3aac64d7e8595af340100d0c2
Shukaro <shukaro@gmail.com>
2014-08-09 13:42:48 -0400

    Fix sickles

--- src/main/java/shukaro/artifice/item/ItemSickle.java
+++ src/main/java/shukaro/artifice/item/ItemSickle.java
@@ -3,6 +3,7 @@ package shukaro.artifice.item;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockBush;
 import net.minecraft.block.BlockFlower;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import net.minecraftforge.common.IPlantable;
 import shukaro.artifice.ArtificeConfig;
 import shukaro.artifice.ArtificeCore;
 import shukaro.artifice.ArtificeRegistry;
@@ -102,7 +104,7 @@ public class ItemSickle extends ItemTool
         Block block = coord.getBlock(world);
         int radius = getRadius(this.toolMaterial);
 
-        if (block instanceof BlockFlower)
+        if (block instanceof IPlantable)
         {
             int count = 0;
             for (int i = -radius; i < radius; i++)
@@ -111,7 +113,7 @@ public class ItemSickle extends ItemTool
                 {
                     Block b = world.getBlock(x + i, y, z + k);
                     int meta = world.getBlockMetadata(x + i, y, z + k);
-                    if (b instanceof BlockFlower && coord.getDistance(x + i, y, z + k) <= radius && b.canHarvestBlock(player, meta))
+                    if (b instanceof IPlantable && coord.getDistance(x + i, y, z + k) <= radius && b.canHarvestBlock(player, meta))
                     {
                         b.harvestBlock(world, player, x + i, y, z + k, meta);
                         world.setBlockToAir(x + i, y, z + k);
@@ -121,8 +123,7 @@ public class ItemSickle extends ItemTool
             }
             if (!world.isRemote)
                 world.playSoundEffect((double) x + 0.5D, (double) y + 0.5D, (double) z + 0.5D, block.stepSound.getBreakSound(), 1.0F, world.rand.nextFloat() * 0.1F + 0.9F);
-            stack.damageItem(count / 4, player);
-            if (stack.stackSize <= 0)
+            if (stack.attemptDamageItem(count / 2, itemRand))
                 player.destroyCurrentEquippedItem();
             return true;
         }

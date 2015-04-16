0a74c7a0954b89ea080ec780cc74d8943aef8dd7
Shukaro <shukaro@gmail.com>
2015-04-16 18:42:43 -0400

    stop caring about the anvil at all

--- src/main/java/shukaro/artifice/event/ArtificeEventHandler.java
+++ src/main/java/shukaro/artifice/event/ArtificeEventHandler.java
@@ -71,13 +71,4 @@ public class ArtificeEventHandler
             }
         }
     }
-
-    @SubscribeEvent
-    public void onAnvil(AnvilRepairEvent event)
-    {
-        if (event.output == null || event.output.getItem() == null || event.output.getItem().getUnlocalizedName() == null)
-            return;
-        else if (event.output.getItem().getUnlocalizedName().contains("tile.artifice.attunedredstone") && event.output.hasTagCompound())
-            event.output.getTagCompound().removeTag("RepairCost");
-    }
 }
